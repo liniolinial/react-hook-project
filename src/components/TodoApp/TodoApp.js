d8af23c2872a2700c0c1476eade1f5181914e957
Lynn Kim <lynn.kim@brandung.de>
2023-07-28 15:21:18 +0200

    updated material ui

--- src/components/TodoApp/TodoApp.js
+++ src/components/TodoApp/TodoApp.js
@@ -1,9 +1,28 @@
 import React from "react";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Grid from "@mui/material/Grid";
 
 export default function Todo() {
   return (
     <div>
       <h1>TODOS</h1>
+      <Paper
+        style={{
+          padding: 0,
+          margin: 0,
+          height: "100vh",
+          backgroundColor: "fafafa",
+        }}
+        elevation={0}>
+        <AppBar color='primary' postition='static' style={{ height: "64px" }}>
+          <Toolbar>
+            <Typography color='inherit'>TODOS WITH HOOKS</Typography>
+          </Toolbar>
+        </AppBar>
+      </Paper>
     </div>
   );
 }
