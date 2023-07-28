fe36934c13e659ce01221526a174fc2c2229de7a
Lynn Kim <lynn.kim@brandung.de>
2023-07-28 14:10:13 +0200

    create todoapp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 // import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import TodoApp from "./components/TodoApp/TodoApp";
+
+import "./App.scss";
 // import { v4 as uuidv4 } from 'uuid';
 // uuidv4();
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <TodoApp />
+    </div>
+  );
 }
 
 export default App;
